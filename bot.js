f5ecf94250ae60102f941528d25195374329129c
Larry Faudree <lfaudreejr@gmail.com>
2019-11-14 13:14:36 -0500

    use yo botkit

--- bot.js
+++ bot.js
@@ -2,49 +2,61 @@
 // |__) /  \  |  |__/ |  |  
 // |__) \__/  |  |  \ |  |  
 
-// This is the main file for the starter-slack bot.
+// This is the main file for the gifcat bot.
 
 // Import Botkit's core features
-const { Botkit } = require('botkit');
-const { BotkitCMSHelper } = require('botkit-plugin-cms');
+const {
+  Botkit
+} = require('botkit');
+const {
+  BotkitCMSHelper
+} = require('botkit-plugin-cms');
 
 // Import a platform-specific adapter for slack.
 
-const { SlackAdapter, SlackMessageTypeMiddleware, SlackEventMiddleware } = require('botbuilder-adapter-slack');
+const {
+  SlackAdapter,
+  SlackMessageTypeMiddleware,
+  SlackEventMiddleware
+} = require('botbuilder-adapter-slack');
 
-const { MongoDbStorage } = require('botbuilder-storage-mongodb');
+const {
+  MongoDbStorage
+} = require('botbuilder-storage-mongodb');
 
 // Load process.env values from .env file
 require('dotenv').config();
 
 let storage = null;
 if (process.env.MONGO_URI) {
-    storage = new MongoDbStorage({
-        url : process.env.MONGO_URI,
-        database: process.env.MONGO_DB
-    });
+  storage = mongoStorage = new MongoDbStorage({
+    url: process.env.MONGO_URI,
+  });
 }
 
 
 
 const adapter = new SlackAdapter({
-    // parameters used to secure webhook endpoint
-    verificationToken: process.env.verificationToken,
-    clientSigningSecret: process.env.clientSigningSecret,  
+  // REMOVE THIS OPTION AFTER YOU HAVE CONFIGURED YOUR APP!
+  enable_incomplete: true,
 
-    // auth token for a single-team app
-    botToken: process.env.botToken,
+  // parameters used to secure webhook endpoint
+  verificationToken: process.env.verificationToken,
+  clientSigningSecret: process.env.clientSigningSecret,
 
-    // credentials used to set up oauth for multi-team apps
-    clientId: process.env.clientId,
-    clientSecret: process.env.clientSecret,
-    scopes: ['bot', 'incoming-webhook'], 
-    redirectUri: process.env.redirectUri,
- 
-    // functions required for retrieving team-specific info
-    // for use in multi-team apps
-    getTokenForTeam: getTokenForTeam,
-    getBotUserByTeam: getBotUserByTeam,
+  // auth token for a single-team app
+  botToken: process.env.botToken,
+
+  // credentials used to set up oauth for multi-team apps
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  scopes: ['bot', 'incoming-webhook'],
+  redirectUri: process.env.redirectUri,
+
+  // functions required for retrieving team-specific info
+  // for use in multi-team apps
+  getTokenForTeam: getTokenForTeam,
+  getBotUserByTeam: getBotUserByTeam,
 });
 
 // Use SlackEventMiddleware to emit events that match their original Slack event types.
@@ -55,60 +67,71 @@ adapter.use(new SlackMessageTypeMiddleware());
 
 
 const controller = new Botkit({
-    
-    webhook_uri: '/api/messages',
+  webhook_uri: '/api/messages',
 
-    adapter: adapter,
+  adapter: adapter,
 
-    storage
+  storage
 });
 
 if (process.env.cms_uri) {
-    controller.usePlugin(new BotkitCMSHelper({
-        cms_uri: process.env.cms_uri,
-        token: process.env.cms_token,
-    }));
+  controller.usePlugin(new BotkitCMSHelper({
+    uri: process.env.cms_uri,
+    token: process.env.cms_token,
+  }));
 }
 
 // Once the bot has booted up its internal services, you can use them to do stuff.
 controller.ready(() => {
 
-    // load traditional developer-created local custom feature modules
-    controller.loadModules(__dirname + '/features');
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(__dirname + '/features');
 
-    /* catch-all that uses the CMS to trigger dialogs */
-    if (controller.plugins.cms) {
-        controller.on('message,direct_message', async (bot, message) => {
-            let results = false;
-            results = await controller.plugins.cms.testTrigger(bot, message);
+  /* catch-all that uses the CMS to trigger dialogs */
+  if (controller.plugins.cms) {
+    controller.on('message,direct_message', async (bot, message) => {
+      let results = false;
+      results = await controller.plugins.cms.testTrigger(bot, message);
 
-            if (results !== false) {
-                // do not continue middleware!
-                return false;
-            }
-        });
-    }
+      if (results !== false) {
+        // do not continue middleware!
+        return false;
+      }
+    });
+  }
 
 });
+
+
 
 controller.webserver.get('/', (req, res) => {
 
-    res.send(`This app is running Botkit ${ controller.version }.`);
+  res.send(`This app is running Botkit ${ controller.version }.`);
 
 });
 
+
+
+
+
+
 controller.webserver.get('/install', (req, res) => {
-    // getInstallLink points to slack's oauth endpoint and includes clientId and scopes
-    res.redirect(controller.adapter.getInstallLink());
+  // getInstallLink points to slack's oauth endpoint and includes clientId and scopes
+  res.redirect(controller.adapter.getInstallLink());
 });
 
 controller.webserver.get('/install/auth', async (req, res) => {
   try {
-    const results = await controller.adapter.validateOauthCode(req.query.code)
+    const results = await controller.adapter.validateOauthCode(req.query.code);
 
-    console.log('FULL OAUTH DETAILS', results)
+    console.log('FULL OAUTH DETAILS', results);
 
     // Store token by team in bot state.
+    tokenCache[results.team_id] = results.bot.bot_access_token;
+
+    // Capture team to bot id
+    userCache[results.team_id] = results.bot.bot_user_id;
+
     storage.write({
       [results.team_id]: {
         bot_access_token: results.bot.bot_access_token,
@@ -116,27 +139,53 @@ controller.webserver.get('/install/auth', async (req, res) => {
       },
     })
 
-    res.json('Success! Bot installed.')
+    res.json('Success! Bot installed.');
+
   } catch (err) {
-    console.error('OAUTH ERROR:', err)
-    res.status(401)
-    res.send(err.message)
+    console.error('OAUTH ERROR:', err);
+    res.status(401);
+    res.send(err.message);
   }
-})
+});
+
+let tokenCache = {};
+let userCache = {};
+
+if (process.env.TOKENS) {
+  tokenCache = JSON.parse(process.env.TOKENS);
+}
+
+if (process.env.USERS) {
+  userCache = JSON.parse(process.env.USERS);
+}
 
 async function getTokenForTeam(teamId) {
+  if (tokenCache[teamId]) {
+    return new Promise((resolve) => {
+      setTimeout(function () {
+        resolve(tokenCache[teamId]);
+      }, 150);
+    });
+  }
   const team = await storage.read([teamId]);
   if (team && team.bot_access_token) {
-      return team.bot_access_token
+    return team.bot_access_token
   } else {
-      console.error('Team not found in tokenCache: ', teamId);
+    console.error('Team not found in tokenCache: ', teamId);
   }
 }
 
 async function getBotUserByTeam(teamId) {
+  if (userCache[teamId]) {
+    return new Promise((resolve) => {
+      setTimeout(function () {
+        resolve(userCache[teamId]);
+      }, 150);
+    });
+  }
   const team = await storage.read([teamId]);
   if (team && team.bot_user_id) {
-      return team.bot_user_id
+    return team.bot_user_id
   } else {
     console.error('Team not found in userCache: ', teamId);
   }
